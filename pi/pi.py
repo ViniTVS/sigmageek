b1760f5c66b6f3a0d60422248aa2bae604445d72
Vinícius Teixeira Vieira dos Santos <vinisantos185@gmail.com>
2022-09-02 16:04:24 -0300

    Set sleep on too many API calls
    Signed-off-by: Vinícius Teixeira Vieira dos Santos <vinisantos185@gmail.com>

--- pi/pi.py
+++ pi/pi.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import math
+import sys
 import requests
 import logging
+import time
 
 # check if number is prime (https://geekflare.com/prime-number-in-python/)
 def is_prime(n):
@@ -18,6 +20,22 @@ def palindrome(array):
             return False
     return True
 
+
+def getPiDigits(start, digits = 1000):
+    url = "https://api.pi.delivery/v1/pi?start="+ str(start) + "&numberOfDigits=" + str(digits)
+    response = requests.get(url, timeout=10)
+    
+    pi_string = ""
+    try:
+        response = requests.get(url)
+        pi_string = response.json()['content']
+    except:
+        # maybe I'm making too many requests, they need a time
+        time.sleep(5)
+        return getPiDigits(start)
+
+    return pi_string
+
 # turn the array numbers into an actual number
 def arrayToNum(array):
     string = ""
@@ -28,13 +46,9 @@ def arrayToNum(array):
     return int(string)
 
 def main():
-    # some logging fro debugg
-    logging.basicConfig(filename='pi.log', encoding='utf-8', level=logging.DEBUG)
-    logging.debug('This message should go to the log file')
     # get pi digits using Google's pi API
-    url = "https://api.pi.delivery/v1/pi?start=0&numberOfDigits=1000"
-    response = requests.get(url)
-    pi_string = response.json()['content']
+    start_num = 0
+    pi_string = getPiDigits(start_num)
     # array to help find palindromes
     array = []
     # just populate the array with the first 21 digits
@@ -43,23 +57,18 @@ def main():
     #check if the 1000 first digits are palindrome
     for i in range(21, len(pi_string)):
         if (palindrome(array)):
-            val = arrayToNum(array)
-            if is_prime(val):
+            if is_prime(arrayToNum(array)):
                 print(val)
                 return
+        # if it's not what we want, remove the first value from the array and insert another
+        array.pop(0)
+        array.append(pi_string[i])
+
     # for every char in the pi_string
-    start_num = 0
-    logging.debug("Start_num: " + str(start_num))
-    logging.debug("Digits: " + pi_string)
     while True:
         # get new digits
         start_num += 1000
-        url = "https://api.pi.delivery/v1/pi?start="+ str(start_num) + "&numberOfDigits=1000"
-        response = requests.get(url)
-        pi_string = response.json()['content']
-
-        logging.debug("Start_num: " + str(start_num))
-        logging.debug("Digits: " + pi_string)
+        pi_string = getPiDigits(start_num)
 
         for i in range(len(pi_string)):
             if (palindrome(array)):
